cb690282ac5143c1b37152fac75d85115736f9a0
patrickchow0803 <patrickchow0803@gmail.com>
2021-02-28 20:54:22 -0500

    Set up a listener for the messages. Also fixed all versioning problems.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -10,6 +11,19 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(10),
           child: Text('This Works'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/fO5XNCegkgRC5e5dyWPa/messages')
+              .snapshots()
+              .listen((event) {
+            event.docs.forEach((element) {
+              print(element['text']);
+            });
+          });
+        },
       ),
     );
   }
